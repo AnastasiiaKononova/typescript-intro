cea3c42655e3165bcfe03033dda0a41f86c21067
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2025-06-04 16:06:21 +0300

    Buying alcohol practice task resolve

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,40 @@
-const firstName: string = "John";
-const lastName: string = "Doe";
+const hasDriverLicence: boolean = true;
 
-const pi: number = 3.14;
-
-console.log(`${firstName} ${lastName}`);
-
-// // + Вивести 10 консоль.логів у консоль
-
-for (let i = 0; i < 10; i++) {
-  console.log(`Число з циклу for - ${i}`);
+if (hasDriverLicence) {
+  console.log("User can to drive auto");
+} else {
+  console.log("User can't to drive auto");
 }
 
-// + Вивести тільки парні
+/*
 
-for (let i = 0; i < 10; i++) {
-  if (i % 2 === 0) {
-    console.log(`Число з циклу for - ${i}`);
+Задача:
+
+Створіть форму з двома інпутами в яких запитуйте ім'я користувача і його вік.
+У формі має бути кнопка "Купити алкоголь".
+
+Відповідно, якщо у користувача вік легальний - ви виводите повідомлення у верстку з повідомленням про те,
+що алкоголь купувати дозволено.
+Якщо користувач не досягнув відповідного віку - виводите у верстку повідомлення про те,
+що алкоголь купувати не дозволено
+
+*/
+
+const form = document.querySelector("#alcohol-form") as HTMLFormElement;
+const messageDiv = document.querySelector("#message") as HTMLDivElement;
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const nameInput = document.querySelector("#name") as HTMLInputElement;
+  const ageInput = document.querySelector("#age") as HTMLInputElement;
+
+  const name: string = nameInput.value;
+  const age: number = Number(ageInput.value);
+
+  if (age >= 18) {
+    messageDiv.textContent = `${name}, Вам дозволено купувати алкоголь.`;
+  } else {
+    messageDiv.textContent = `${name}, Вам заборонено купувати алкоголь.`;
   }
-}
-
-console.log(this);
-console.dir(document);
-
-
-// Задача: створити невеличкий додаток, який може обчислити факторіал числаAdd commentMore actions
-// Факторіал числа - добуток всіх цілих чисел від 1 до цього числа включно
-// 5! = 1 * 2 * 3 * 4 * 5 = 120
-
-const form = document.querySelector('#factorial-form') as HTMLFormElement;
-const stepList = document.querySelector('#factorial-steps') as HTMLOListElement;
-
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    // Очищаємо список перед виведенням нових результатів
-    stepList.textContent = '';
-
-    // Отримати введене число
-    const numberInput = document.querySelector('#number') as HTMLInputElement;
-    const number: number = Number(numberInput.value);
-
-    // Обчисляємо факторіал та додавання кожної ітерації до списку
-    let factorial: number = 1;
-    for(let i = 1; i <= number; i++) {
-        factorial *= i;
-
-        const listItem: HTMLLIElement = document.createElement('li');
-        listItem.textContent = `${factorial}`;
-        stepList.appendChild(listItem);
-    }
 });
